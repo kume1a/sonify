6c8bed2645a1840372b31b4c42b7438dab21f16f
kume1a <kumela011@gmail.com>
2024-10-14 22:46:25 +0400

    set loglevel to warn on non-debug build

--- lib/app/configuration/configure_logging.dart
+++ lib/app/configuration/configure_logging.dart
@@ -5,7 +5,7 @@ import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 
 void configureLogging() {
-  Logger.root.level = kDebugMode ? Level.ALL : Level.OFF;
+  Logger.root.level = kDebugMode ? Level.ALL : Level.WARNING;
   Logger.root.onRecord.listen((record) {
     log('${record.level.name}: ${record.time}: ${record.message}');
 
